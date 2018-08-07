3dd73d229f0e0fa338ddf3474bbf0b3548e82919
Avinash Jadhav <avinash.jadhav@gslab.com>
2018-08-07 16:12:58 +0530

    Updated kube files to change pod label

--- src/pgsql/bin/postgres/primary/entrypoint.sh
+++ src/pgsql/bin/postgres/primary/entrypoint.sh
@@ -8,6 +8,22 @@ psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE ROLE $REPLIC
 echo ">>> Creating replication db '$REPLICATION_DB'"
 createdb $REPLICATION_DB -O $REPLICATION_USER
 
+### update pod label for master : role: primary
+cat > patch.json <<EOF
+[ 
+ { 
+ "op": "add", "path": "/metadata/labels/role", "value": "primary" 
+ } 
+]
+EOF
+
+KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
+curl -k --header "Authorization: Bearer $KUBE_TOKEN" --request PATCH --data "$(cat patch.json)" \
+  -H "Content-Type:application/json-patch+json" \
+  https://kubernetes.default.svc:443/api/v1/namespaces/${MY_POD_NAMESPACE}/pods/${MY_POD_NAME}
+
+####
+
 #TODO: make it more flexible, allow set of IPs
 # Why db_name='replication' - https://dba.stackexchange.com/questions/82351/postgresql-doesnt-accept-replication-connection
 echo "host replication $REPLICATION_USER 0.0.0.0/0 md5" >> $PGDATA/pg_hba.conf
--- src/pgsql/bin/postgres/standby/entrypoint.sh
+++ src/pgsql/bin/postgres/standby/entrypoint.sh
@@ -23,6 +23,23 @@ fi
 
 rm -f $MASTER_ROLE_LOCK_FILE_NAME # that file should not be here anyways
 
+### update pod label for standby : role: standby
+cat > patch.json <<EOF
+[
+ {
+ "op": "add", "path": "/metadata/labels/role", "value": "standby"
+ }
+]
+EOF
+
+KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
+curl -k --header "Authorization: Bearer $KUBE_TOKEN" --request PATCH --data "$(cat patch.json)" \
+  -H "Content-Type:application/json-patch+json" \
+  https://kubernetes.default.svc:443/api/v1/namespaces/${MY_POD_NAMESPACE}/pods/${MY_POD_NAME}
+
+####
+
+
 postgres_configure
 
 echo ">>> Starting postgres..."
